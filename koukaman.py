1dbaaa5552854831103558773bd7cea80539fee2
Okamoto <c0a2405812@edu.teu.ac.jp>
2025-06-03 08:19:30 +0900

    最終機能追加

--- koukaman.py
+++ koukaman.py
@@ -25,7 +25,7 @@ def check_bound(obj_rct: pg.Rect, ) -> bool:
     戻り値：オブジェクトの中心座標が白いかどうかの判定、もしくは
     横方向，縦方向のはみ出し判定結果（画面外、壁：False）
     """
-    # ↓ここに衝突判定を追加↓
+    # 衝突判定
     hantei =  True
     if screen.get_at((obj_rct.centerx,obj_rct.centery)) == (255,255,255,255):
         hantei = False
@@ -44,7 +44,6 @@ def check_bound(obj_rct: pg.Rect, ) -> bool:
         # Collision = 3
     return hantei # Collision
 
-    # ↑ここに衝突判定を追加↑
 
 def check_bound2(rct: pg.Rect) -> tuple[bool, bool]:
     """
@@ -59,6 +58,7 @@ def check_bound2(rct: pg.Rect) -> tuple[bool, bool]:
     if rct.top < 0 or height < rct.bottom:  # 画面外だったら
         tate = False
     return yoko, tate
+
 
 class Score:
     """
@@ -81,16 +81,19 @@ class Enemy(pg.sprite.Sprite):
     """
     敵（ゴースト）
     """
-    def __init__(self):
+    def __init__(self, x :list ,y :list):
         """
         敵初期化。Surfaceを作成
         引数無し
         """
         super().__init__()
         self.image = pg.transform.rotozoom(pg.image.load("fig/ghost.png"),0,0.08)
+        self.image_normal = self.image.copy()
+        self.image_hyper = self.image.copy()
+        self.image_hyper.fill((255, 255, 0, 0), special_flags=pygame.BLEND_RGB_MULT)
         self.rect = self.image.get_rect()
-        self.rect.centerx = random.randint(0,640)
-        self.rect.centery = random.randint(0,480)
+        self.rect.centerx = random.choice(x)
+        self.rect.centery = random.choice(y)
         self.vx = 4
         self.vy = 4
         
@@ -99,13 +102,36 @@ class Enemy(pg.sprite.Sprite):
         ゴーストの位置を更新
         引数 screen :画面Surface
         """
-        self.rect.move_ip(self.vx, self.vy)  # 爆弾の移動
+        # こうかとんが無敵状態でのゴーストの見た目変更
+        if bird.hyper == 1:
+            self.image = self.image_hyper  # ゴーストの描画（無敵時）
+            # ゴーストのスピードを半減
+            if self.vx < 0:
+                self.vx = -2
+            else:
+                self.vx = 2
+            if self.vy < 0:
+                self.vy = -2
+            else:
+                self.vy = 2
+        else:
+            self.image = self.image_normal  # ゴーストの描画（通常時）
+            # ゴーストのスピードを通常にする
+            if self.vx < 0:
+                self.vx = -4
+            else:
+                self.vx = 4
+            if self.vy < 0:
+                self.vy = -4
+            else:
+                self.vy = 4
+        self.rect.move_ip(self.vx, self.vy)  # ゴーストの移動
         yoko, tate = check_bound2(self.rect)
         if not yoko:  # 左右どちらかにはみ出ていたら
             self.vx *= -1
         if not tate:  # 上下どちらかにはみ出ていたら
             self.vy *= -1
-        screen.blit(self.image, self.rect)  # 爆弾の描画
+        screen.blit(self.image,self.rect) 
         
 
 class Bird(pg.sprite.Sprite):
@@ -144,6 +170,10 @@ class Bird(pg.sprite.Sprite):
         self.rect.center = xy
         self.speed = 4
         self.hyper = 0
+        # こうかとん点滅の時間
+        self.time = 0
+        #  こうかとん点滅のフラグ（1なら点滅する）
+        self.tenmetu_flag = 0
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -151,12 +181,28 @@ class Bird(pg.sprite.Sprite):
         引数1 num：こうかとん画像ファイル名の番号
         引数2 screen：画面Surface
         """
-        self.image = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 0.9)
+        self.image = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 0.6)
         screen.blit(self.image, self.rect)
 
+    def tenmetu_img(self, num :int):
+        """
+        こうかとんが敵に衝突したとき、無敵時間の分だけ点滅する
+        引数 num :無敵時間int型
+        """
+        self.time = num
+        # こうかとん点滅
+        if self.time == 0:
+            self.tenmetu_flag = 0
+            for img in self.imgs:
+                self.imgs[img].set_alpha(255)
+        elif self.time%2 == 0 and self.tenmetu_flag == 1:
+            self.image.set_alpha(0)
+        elif self.time%2 == 1 and self.tenmetu_flag == 1:
+            self.image.set_alpha(255)
+        
     def update(self, key_lst: list[bool], screen: pg.Surface): # Collision
         """
-        押下キーに応じてこうかとんを移動させる
+        押下キーに応じてこうかとんを移動させ、表示を管理する
         引数1 key_lst：押下キーの真理値リスト
         引数2 screen：画面Surface
         """
@@ -296,10 +342,42 @@ class Life:
         for i in range(self.value):
             screen.blit(self.image, (10 + i * 35, height - 30))
 
+class Time:
+    """
+    制限時間のクラス
+    """
+    def __init__(self):
+        """
+        制限時間Surface作成＆表示
+        引数無し
+        """
+        self.clock = pg.time.get_ticks()
+        self.font = pg.font.Font(None, 35)
+        self.color = (0, 0, 0)
+        self.value = 0
+        self.start_time = 100  # 制限時間
+        self.start_time += 5  # ゲーム開始BGM用の余分時間
+        self.image = self.font.render(f"Time: {self.start_time}", 0, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = width/1.25, 15
+
+    def update(self,screen :pg.Surface):
+        """
+        制限時間の更新。制限時間が10秒以下なら、Surfaceを赤色に変更
+        引数 screen :画面Surface
+        """
+        self.clock = self.start_time - (pg.time.get_ticks()//1000) 
+        if self.clock <= 10:
+            self.image = self.font.render(f"Time: {self.clock}", 0, RED)
+        else:
+            self.image = self.font.render(f"Time: {self.clock}", 0, self.color)
+        screen.blit(self.image, self.rect)
+
+
 # ウィンドウ設定
 width, height = 640, 480
 screen = pg.display.set_mode((width, height))
-pg.display.set_caption("Pac-Man")
+pg.display.set_caption("Kouka-Man")
 
 # フォントの設定
 font = pg.font.Font(None, 74)
@@ -317,10 +395,13 @@ worlds.draw(screen)
 bird = Bird(3,(width//2,height//2))
 
 # 敵のグループ作成
-# 敵人数設定
+enemy_num = 5  # 敵の基本出現数
+# 敵のx座標、y座標のリスト
+X_list = [i for i in range(width) if i<width//3 or i>(width//3)*2]
+Y_list = [i for i in range(height) if i<height//3 or i>(height//3)*2]
 emys = pg.sprite.Group()
-for _ in range(5):
-    emys.add(Enemy())
+for _ in range(enemy_num):
+    emys.add(Enemy(X_list,Y_list))
 
 # コインとアイテムの設定
 coin_size = 15
@@ -364,6 +445,12 @@ bgm.update()
 score = Score()
 # 残機の設定
 life = Life()
+# 制限時間の設定
+time_l = Time()
+
+# 30秒ごとに、敵の数が一定以上少ないのならば、一定数になるように敵追加イベントを発生する
+Enemy_event = pg.USEREVENT + 1 
+pg.time.set_timer(Enemy_event, 30000)
 
 # メインの処理
 while running:
@@ -386,8 +473,9 @@ while running:
             items.remove(item)
             bird.hyper = 1
             bird.speed = 6
-            item_frame = 500
+            item_frame = 300
 
+    # 無敵時間の減算
     if life.count > 0:
         life.count -= 1
 
@@ -397,7 +485,9 @@ while running:
             #---------ライフを１減らし、０になったらゲームオーバー---------
             if life.count == 0:
                 life.value -= 1
-                life.count = fps  # 無敵時間1秒
+                life.count = fps*2  # 無敵時間2秒
+                bird.change_img(8, screen)  # こうかとん悲しみエフェクト
+                bird.tenmetu_flag = 1  # こうかとん点滅フラグ
                 if life.value == 0:
                     game_over = True
                     running = False
@@ -406,11 +496,10 @@ while running:
             enemy.kill()
             score.value += 50
 
-    # コインがすべて収集された場合
+    # コインがすべて収集された場合にゲーム終了
     if not coins:
         game_clear = True
         running = False
-
 
     # 画面の描画
     screen.fill(BLACK)
@@ -428,8 +517,19 @@ while running:
         bird.hyper = 0
         bird.speed = 4
 
+    # 制限時間が0になれば終了
+    if time_l.clock == 0:
+        game_over = True
+        running = False
+
     # こうかとんのupdate
     bird.update(key_lst,screen)
+    # 敵の数が基本数より少ないかつ敵追加イベントが発生しているなら敵追加
+    if len(emys) < enemy_num:
+        if event.type == Enemy_event:
+            emys.add(Enemy(X_list,Y_list))
+        else:
+            pass
     # 敵のupdate
     emys.update()
     emys.draw(screen)
@@ -437,6 +537,11 @@ while running:
     score.update(screen)
     # 残機のupdate
     life.update(screen)
+    # 制限時間のupdate
+    time_l.update(screen)
+    # こうかとん点滅
+    if bird.tenmetu_flag == 1:
+        bird.tenmetu_img(life.count)
     pg.display.update()
     clock.tick(fps)
 
